454fb003355f96cb2a234927005370d63ec7408d
jiexinghan <2420499179@qq.com>
2025-02-16 12:46:50 +0800

    feat: error handle middeleware and cookieParser

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.route.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
